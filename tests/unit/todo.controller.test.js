95c98ad988fb1d1c208109e14c4d16cf726d843d
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-22 10:51:57 -0300

    test: ensure creation of new Todo

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,8 +1,40 @@
 const TodoController = require('../../src/controllers/todo.controller')
+const TodoModel = require('../../src/model/todo.model')
+const httpmocks = require('node-mocks-http')
+
+const newTodo = require('../mock-data/new-todo.json')
+
+TodoModel.create = jest.fn();
+let request, response, next;
+
+
+beforeEach(() => {
+  
+  request = httpmocks.createRequest()
+  response = httpmocks.createResponse()
+  next = null
+})
 
 describe('TodoController', () => {
+    beforeEach(() => {
+      request.body = newTodo
+    })
+
     it('should have a createTodo function', () => {
-      console.log(typeof TodoController.createTodo)
       expect(typeof TodoController.createTodo).toBe('function')
+    })
+    it('should call TodoModel.create', () => {
+      TodoController.createTodo(request, response, next);
+      expect(TodoModel.create).toBeCalledWith(newTodo)
+    })
+    it('should return 201 response code', () => {
+       TodoController.createTodo(request, response, next)
+       expect(response.statusCode).toBe(201)
+       expect(response._isEndCalled()).toBeTruthy()
+    })
+    it('should return json body in response', () => {
+      TodoModel.create.mockReturnValue(newTodo)
+      TodoController.createTodo(request, response, next)
+      expect(response._getJSONData()).toStrictEqual(newTodo)
     })
 })
