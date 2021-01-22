927e80c81ed94c48055eb75b0bff76fb2546a86d
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-22 10:51:50 -0300

    feat: ensure creation of new Todo

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,1 +1,6 @@
-exports.createTodo = () => {}
+const TodoModel = require('../model/todo.model')
+
+exports.createTodo = (request, response, next ) => {
+  const createdModel = TodoModel.create(request.body)
+  return response.status(201).json(createdModel)
+}
