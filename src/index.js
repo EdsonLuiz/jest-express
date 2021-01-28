033fc8dc7d25635dcfeae3e9a98fd66dd61e59ef
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-28 15:13:05 -0300

    refactor: add mongo to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const { json } = require('express')
 
+const mongodb = require('../mongodb/mongodb.connect')
 const todoRotes = require('./routes/todo.routes')
 
+mongodb.connect()
 
 const app = express()
 app.use(cors())
